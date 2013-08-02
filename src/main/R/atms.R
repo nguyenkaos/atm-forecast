5b145f48b6d0d7350a815e7494c5524548204b17
Nick Allen <nick@nickallen.org>
2013-08-02 15:05:52 -0400

    Persisting intermediate scores by ATM and avoiding further calculation if the intermediate scores already exist for an ATM.

--- src/main/R/atms.R
+++ src/main/R/atms.R
@@ -9,7 +9,10 @@ source("clean.R")
 source("train.R")
 source("score.R")
 
-pathToCash = "../resources/cash.rds"
+# create a working directory
+dir.create(path="./work", showWarnings=FALSE)
+
+pathToCash = "./work/cash.rds"
 if(!file.exists(pathToCash)) {
   
   # load the raw input data
@@ -34,22 +37,39 @@ gc()
 
 trainAndScoreByAtm <- function(data) {
   
-  atm <- unique(as.character(data$atm))
-  print(sprintf("training... ATM=%s nrows=%.0f minDate=%s maxDate=%s\n", 
-                atm, 
-                nrow(data),
-                min(data$trandate),
-                max(data$trandate))) 
+  scored <- NULL
+  atm = unique(as.character(data$atm))
+  atmFile = paste("./work/atm-", atm, ".rds", sep="")
   
-  # train and score the data
-  fit <- trainer(data, 
-                 form=usage ~ sin(dayOfYear*2*pi) + sin(dayOfSemiYear*2*pi) + sin(dayOfQuarter*2*pi) + dayOfWeek + weekOfMonth + weekOfYear + paydayN + holidayN + eventDistance, 
-                 method="rf", 
-                 defaultTuneGrid=expand.grid(.mtry=max(floor(ncol(data)/3), 1)), 
-                 p=ymd("2013-05-15"))
+  # the scores were already calculated for this ATM
+  if(file.exists(atmFile)) {
+    print(sprintf("loading existing scores from %s", atmFile))
+    scored <- readRDS(atmFile)
   
-  if(!is.null(fit)) {
-    scored <- score(data, fit)
+  # train and score the ATM
+  } else {
+    print(sprintf("training... ATM=%s nrows=%.0f minDate=%s maxDate=%s\n", 
+                  atm, 
+                  nrow(data),
+                  min(data$trandate),
+                  max(data$trandate))) 
+    
+    # train the model
+    fit <- trainer(data, 
+                   form=usage ~ sin(dayOfYear*2*pi) + sin(dayOfSemiYear*2*pi) + sin(dayOfQuarter*2*pi) + dayOfWeek + weekOfMonth + weekOfYear + paydayN + holidayN + eventDistance, 
+                   method="rf", 
+                   defaultTuneGrid=expand.grid(.mtry=max(floor(ncol(data)/3), 1)), 
+                   p=ymd("2013-05-15"))
+    
+    # score the model
+    if(!is.null(fit)) {
+      scored <- score(data, fit)
+    
+    } else {
+      # not enough data to train a model
+      scored <- data.frame()
+      print(sprintf("not enough data to score ATM: %s", atm)) 
+    }
     
     # print diagnostics
     test <- subset(scored, isTest=TRUE)
@@ -57,13 +77,16 @@ trainAndScoreByAtm <- function(data) {
                   RMSE(test$usageHat, test$usage, na.rm=T),
                   R2(test$usageHat, test$usage, na.rm=T),
                   100*sum(test$score, na.rm=T)/(nrow(test)*2)))
-  } else {
-    scored <- data.frame()
-    print(sprintf("not enough data to score ATM: %s", atm)) 
   }
+  
+  
+
+  
+
+
 
   # save off the scored data
-  saveRDS(scored, paste("../resources/atm-", atm, ".rds", sep=""))
+  saveRDS(scored, paste("./work/atm-", atm, ".rds", sep=""))
   keep(scored)
 
   return(scored)
@@ -71,12 +94,12 @@ trainAndScoreByAtm <- function(data) {
   
 # train and score the model by atm
 scoreByAtm <- ddply(cash, "atm", trainAndScoreByAtm, .parallel=FALSE)
-saveRDS(scoreByAtm, "../resources/scoreByAtm.rds")  
+saveRDS(scoreByAtm, "./work/scoreByAtm.rds")  
 keep(scoreByAtm)
 
 # view score by day
 scoreByDate <- ddply(scoreByAtm, ~trandate, summarise, totalScore=sum(score), .parallel=FALSE)
-saveRDS(scoreByDate, "../resources/scoreByDate.rds")
+saveRDS(scoreByDate, "./work/scoreByDate.rds")
 
 
 
