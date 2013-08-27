190b43f935507f313acf9e423dad1df02ddc66f9
Nick Allen <nick@nickallen.org>
2013-08-27 06:35:47 -0400

    Making progress toward tuning the cluster size based on forecast.

--- src/main/R/forecast/utils/multicore.R
+++
@@ -1,8 +0,0 @@
-############################################################################
-# This file should be sourced to enable some level of parallelism, especially
-# when using Plyr.
-############################################################################
-
-library("doMC")
-registerDoMC(cores = 8)
-
