92437bef1c3d81f9f81e694246d95a408e5a4efc
Nick Allen <nick@nickallen.org>
2013-09-05 18:20:01 -0400

    Added trend summaries over entire fleet.

--- src/main/R/forecast/gbm.R
+++ src/main/R/forecast/gbm.R
@@ -5,34 +5,41 @@
 # metrics.  
 ##################################################################
 trainAndScore <- function(data) {
-    atm <- unique(as.character(data$atm))
+  atm <- unique(as.character(data$atm))
+  
+  # build and cache the fitted model
+  fit <- cache(sprintf("fit-%s", atm), { 
+    splitAt <- ymd("2013-07-01")
     
-    # build and cache the fitted model
-    fit <- cache(sprintf("fit-%s", atm), { 
-        splitAt <- ymd("2013-07-01")
-        
-        # train the model
-        fit <- trainer(data, 
-                       #form=usage ~ .,
-                       form=usage ~ 
-                           dayOfYear + dayOfSemiYear + dayOfQuarter + dayOfWeek + 
-                           weekOfMonth + weekOfYear + paydayN + holidayN + 
-                           eventDistance + trandateN + 
-                           woyMean + woyMin + woyMax + woySd + 
-                           moyMean + moyMin + moyMax + moySd + 
-                           dowMean + dowMin + dowMax + dowSd + 
-                           womMean + womMin + womMax + womSd + 
-                           qMean + qMin + qMax + qSd,
-                       p=splitAt,
-                       method="gbm", 
-                       defaultTuneGrid = expand.grid(.interaction.depth=2, .n.trees=50, .shrinkage=0.1), 
-                       verbose=F, 
-                       distribution="poisson")
-    })
-    
-    # score the model
-    all <- score(data, fit)
-    logScore(subset(all, !(trandateN %in% fit$trainingData$trandateN)))
-    
-    return(all)
+    # train the model
+    fit <- trainer(data, 
+                   form=usage ~ 
+                     trandateN + dayOfYear + dayOfSemiYear + dayOfQuarter + dayOfWeek + 
+                     weekOfMonth + weekOfYear + paydayN + holidayN + eventDistance +
+                     woyMean + woyMin + woyMax + woySd + 
+                     moyMean + moyMin + moyMax + moySd + 
+                     dowMean + dowMin + dowMax + dowSd + 
+                     womMean + womMin + womMax + womSd + 
+                     quMean + quMin + quMax + quSd +
+                     holMean + holMin + holMax + holSd +
+                     payMean + payMin + payMax + paySd +
+                     woyAllMean + woyAllMin + woyAllMax + woyAllSd + 
+                     moyAllMean + moyAllMin + moyAllMax + moyAllSd + 
+                     dowAllMean + dowAllMin + dowAllMax + dowAllSd +  
+                     womAllMean + womAllMin + womAllMax + womAllSd + 
+                     quAllMean + quAllMin + quAllMax + quAllSd + 
+                     holAllMean + holAllMin + holAllMax + holAllSd +  
+                     payAllMean + payAllMin + payAllMax + payAllSd,
+                   p=splitAt,
+                   method="gbm", 
+                   defaultTuneGrid = expand.grid(.interaction.depth=2, .n.trees=50, .shrinkage=0.1), 
+                   verbose=F, 
+                   distribution="poisson")
+  })
+  
+  # score the model
+  all <- score(data, fit)
+  logScore(subset(all, !(trandateN %in% fit$trainingData$trandateN)))
+  
+  return(all)
 }
