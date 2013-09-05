0f17260ad664e905c29a080414e982bf9023ab59
nickwallen <nick@nickallen.org>
2013-09-05 10:29:47 -0400

    Refactored code so that plugging in and switching models is as easy as possible.

--- src/main/R/forecast/forest.R
+++ src/main/R/forecast/forest.R
@@ -1,17 +1,3 @@
-library("plyr")
-library("caret")
-library("lubridate")
-library("randomForest")
-library("gdata")
-
-source("utils/cache.R")
-source("utils/clean.R")
-source("utils/train.R")
-source("utils/score.R")
-
-# settings for parallel execution
-#source("multicore.R")
-parallel = FALSE
 
 ##################################################################
 # Trains and scores a set of data.  Returns the entire input data
@@ -32,21 +18,8 @@ trainAndScore <- function(data) {
                        defaultTuneGrid=expand.grid(.mtry=max(floor(ncol(data)/3), 1)))
         
         # score the model
-        scored <- score(data, fit)
-        return(scored)          
+        scored <- score(data, fit)         
+        loginfo("total score for %s across train & test is %.1f", atm, sum(scored$score))
+        scored
     })
 }
-
-# load, clean and cache the input data
-cash <- cache("cash", clean())
-
-# train and score the model by atm
-scoreByAtm <- ddply(cash, "atm", trainAndScore, .parallel=parallel)
-saveRDS(scoreByAtm, "./work/forest-scoreByAtm.rds")  
-
-# summarize the scores by day
-scoreByDate <- ddply(scoreByAtm, ~trandate, summarise, totalScore=sum(score), .parallel=parallel)
-saveRDS(scoreByDate, "./work/forest-scoreByDate.rds")
-
-
-
