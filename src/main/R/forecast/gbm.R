ed715a7317be12329dd84b444518fa2b9e195b5e
Nick Allen <nick@nickallen.org>
2013-09-06 08:45:30 -0400

    Reorganized the prediction/training formula so that it is more understandable with a larger number of features.

--- src/main/R/forecast/gbm.R
+++ src/main/R/forecast/gbm.R
@@ -5,41 +5,49 @@
 # metrics.  
 ##################################################################
 trainAndScore <- function(data) {
-  atm <- unique(as.character(data$atm))
-  
-  # build and cache the fitted model
-  fit <- cache(sprintf("fit-%s", atm), { 
-    splitAt <- ymd("2013-07-01")
+    atm <- unique(as.character(data$atm))
     
-    # train the model
-    fit <- trainer(data, 
-                   form=usage ~ 
-                     trandateN + dayOfYear + dayOfSemiYear + dayOfQuarter + dayOfWeek + 
-                     weekOfMonth + weekOfYear + paydayN + holidayN + eventDistance +
-                     woyMean + woyMin + woyMax + woySd + 
-                     moyMean + moyMin + moyMax + moySd + 
-                     dowMean + dowMin + dowMax + dowSd + 
-                     womMean + womMin + womMax + womSd + 
-                     quMean + quMin + quMax + quSd +
-                     holMean + holMin + holMax + holSd +
-                     payMean + payMin + payMax + paySd +
-                     woyAllMean + woyAllMin + woyAllMax + woyAllSd + 
-                     moyAllMean + moyAllMin + moyAllMax + moyAllSd + 
-                     dowAllMean + dowAllMin + dowAllMax + dowAllSd +  
-                     womAllMean + womAllMin + womAllMax + womAllSd + 
-                     quAllMean + quAllMin + quAllMax + quAllSd + 
-                     holAllMean + holAllMin + holAllMax + holAllSd +  
-                     payAllMean + payAllMin + payAllMax + payAllSd,
-                   p=splitAt,
-                   method="gbm", 
-                   defaultTuneGrid = expand.grid(.interaction.depth=2, .n.trees=50, .shrinkage=0.1), 
-                   verbose=F, 
-                   distribution="poisson")
-  })
-  
-  # score the model
-  all <- score(data, fit)
-  logScore(subset(all, !(trandateN %in% fit$trainingData$trandateN)))
-  
-  return(all)
+    # build and cache the fitted model
+    fit <- cache(sprintf("fit-%s", atm), { 
+        splitAt <- ymd("2013-07-01")
+        
+        # train the model
+        fit <- trainer(data, 
+                       form=usage ~ 
+                           # date related features
+                           trandateN + dayOfMonth + dayOfYear + dayOfSemiYear + dayOfQuarter + 
+                           dayOfWeek + weekOfMonth + weekOfYear + quarter + monthOfYear + 
+                           
+                           # paydays, holidays, events
+                           paydayN + holidayN + eventDistance +
+                           
+                           # usage trends specific to the ATM
+                           woyMean + woyMin + woyMax + woySd + 
+                           moyMean + moyMin + moyMax + moySd + 
+                           dowMean + dowMin + dowMax + dowSd + 
+                           womMean + womMin + womMax + womSd + 
+                           quMean + quMin + quMax + quSd +
+                           holMean + holMin + holMax + holSd +
+                           payMean + payMin + payMax + paySd +
+                           
+                           # usage trends across all ATMs
+                           woyAllMean + woyAllMin + woyAllMax + woyAllSd + 
+                           moyAllMean + moyAllMin + moyAllMax + moyAllSd + 
+                           dowAllMean + dowAllMin + dowAllMax + dowAllSd +  
+                           womAllMean + womAllMin + womAllMax + womAllSd + 
+                           quAllMean + quAllMin + quAllMax + quAllSd + 
+                           holAllMean + holAllMin + holAllMax + holAllSd +  
+                           payAllMean + payAllMin + payAllMax + payAllSd,
+                       p=splitAt,
+                       method="gbm", 
+                       defaultTuneGrid = expand.grid(.interaction.depth=2, .n.trees=50, .shrinkage=0.1), 
+                       verbose=F, 
+                       distribution="poisson")
+    })
+    
+    # score the model
+    all <- score(data, fit)
+    logScore(subset(all, !(trandateN %in% fit$trainingData$trandateN)))
+    
+    return(all)
 }
