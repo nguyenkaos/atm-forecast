415057cb66329aba268c3b4a21258fd5a1a62d7e
nickwallen <nick@nickallen.org>
2013-09-12 09:12:45 -0400

    The setup.R script now works directly from the command-line

--- src/main/R/common/setup.R
+++ src/main/R/common/setup.R
@@ -1,24 +1,46 @@
+#!/usr/bin/env Rscript
+
+##################################################################
+# Installs a package only if it is not already installed.
+##################################################################
+install <- function(package) {
+    cat(sprintf("checking for %s...\n", package))
+    
+    if(!require(package, character.only=T)) {
+        cat(sprintf("installing %s...\n", package))
+        install.packages(package, dep=T)
+        
+        if(!require(package, character.only=T))
+            stop(sprintf("unable to install package: %s", package))
+    }
+}
 
 ##################################################################
 # Installs all of the packages necessary for forecasting.  The
 # default mirror of 81 is run by Case Western in Ohio.
 ##################################################################
 setup <- function(mirror=81) {
+    cat("installing required packages...\n")
     
     # choose a CRAN mirror site
     chooseCRANmirror(ind=mirror)
     
     # install the packages
-    install.packages(c(
-        "plyr", 
-        "caret", 
-        "lubridate", 
-        "gbm", 
-        "gdata", 
-        "randomForest", 
-        "Hmisc",
-        "logging"
-        "data.table"))
+    cat("FOO!")
+    required = c("plyr", 
+                 "caret", 
+                 "lubridate", 
+                 "gbm", 
+                 "gdata", 
+                 "randomForest", 
+                 "Hmisc",
+                 "logging",
+                 "data.table",
+                 "optparse")
+    sapply(required, install)
+    
+    cat("installation successful\n")
 }
 
+Sys.setenv(HTTP_PROXY="http://proxy.jpmchase.net:8443")
 setup()
