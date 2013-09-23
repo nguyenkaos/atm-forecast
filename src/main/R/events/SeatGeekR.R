516806729b0dde412c3c463a73dad20ec7af5f6c
nickwallen <nick@nickallen.org>
2013-09-23 12:52:01 -0400

    The SeatGeek data is now returned as a flat data frame.

--- src/main/R/events/SeatGeekR.R
+++ src/main/R/events/SeatGeekR.R
@@ -13,32 +13,68 @@ SeatGeekR <- proto(expr={
     new <- function(self, baseURL="http://api.seatgeek.com/2") {
         self$proto(baseURL=baseURL)
     }
-
+    
     # search for events
     events <- function(self, ..., verbose=F) {
-        fromJSON(getForm(paste(self$baseURL, "events", sep="/"), 
-                         .opts=curlOptions(verbose=verbose),
-                         ...))        
+        self$fetch(what="events", ..., verbose=verbose)
     }
     
     # search for performers
     performers <- function(self, ..., verbose=F) {
-        fromJSON(getForm(paste(self$baseURL, "performers", sep="/"), 
-                         .opts=curlOptions(verbose=verbose),
-                         ...))
+        self$fetch("performers", ..., verbose)
     }
     
     # search for venues
     venues <- function(self, ..., verbose=F) {
-        fromJSON(getForm(paste(self$baseURL, "venues", sep="/"), 
-                         .opts=curlOptions(verbose=verbose),
-                         ...))
+        self$fetch("venues", ..., verbose)
     }
     
     # search for taxonomies
     taxonomies <- function(self, ..., verbose=F) {
-        fromJSON(getForm(paste(self$baseURL, "taxonomies", sep="/"), 
-                         .opts=curlOptions(verbose=verbose),
-                         ...))
+        self$fetch("taxonomies", ..., verbose)
     }
+    
+    
+    fetch <- function(self, what, ..., verbose=F) {
+        json <- getForm(paste(self$baseURL, what, sep="/"), .opts=curlOptions(verbose=verbose), ...)
+        asList <- fromJSON(json)
+        to.data.frame(unique.names(asList[[2]], what))
+    }    
 })
+
+##
+# Ensure that each list element has a unique name.
+#
+unique.names <- function(aList, baseName, recursive=T) {
+    # nothing to do; also ends recursion
+    if(length(aList)==0)
+        return(aList)
+    
+    # if the list elements are not named, name them
+    if(is.null(names(aList))) {
+        names(aList) <- paste(baseName, 1:length(aList), sep=".")
+    }
+    
+    if(recursive) {
+        # recursively rename any sub-lists
+        attrNames <- attributes(aList)$names
+        for(i in 1:length(attrNames)) {
+            attr <- aList[[i]]
+            if(is.list(attr)) {
+                aList[[i]] <- unique.names(attr, attrNames[[i]])
+            }
+        }
+    }
+    
+    return(aList)
+}
+
+##
+# 
+# 
+to.data.frame <- function(aList) {
+    as.data.frame(
+        do.call(rbind.fill.matrix, lapply(aList, function(l) t(unlist(l))))
+    )        
+}
+
--- src/main/R/events/test-SeatGeekR.R
+++ src/main/R/events/test-SeatGeekR.R
@@ -1,33 +1,11 @@
 
 
+library("plyr")
+
 # even with stats hard to tell magnitude of the event
 # a performer can contibute to magnitude
 # a venue can contribute to the magnitude
     
 source("SeatGeekR.R")
 geek <- SeatGeekR$new()
-
-# ohio stadium
-#result <- geek$venues(lat=40.0032, lon=-83.0195, range="1mi")
-result <- geek$events(lat=40.0032, lon=-83.0195, range="1mi", datetime_utc.gt="2012-09-07")
-
-# attempt 3
-rows <- result$meta$per_page
-unlist <- unlist(result$events[[2]], recursive=T)
-m <- matrix(unlist, nrow=rows)
-
-
-# attempt 2
-flat <- do.call(c, unlist(result$events, recursive=F))
-eventFields <- c(1, 6, 9, 324) # event id, title, type, datetime_utc
-statsFields <- grep("^stats.*", names(flat))
-venueFields <- grep("^venue.*", names(flat))
-keepers <- sapply(flat, "[", c(eventFields, statsFields, venueFields))
-df <- data.frame(matrix(keepers, nrow=length(result$events), byrow=T))
-
-# attempt 1 - this does work but I have to manually specify all the fields
-events <- result$events
-events <- sapply(events, "[", c("id","title","type","datetime_utc","venue.id","venue.name"))
-df <- data.frame(matrix(events, nrow=result$meta$per_page, byrow=T))
-colnames(df) <- row.names(events)
-
+result <- geek$events(lat=40.0032, lon=-83.0195, range="1mi", datetime_utc.gt="2012-09-07") # ohio stadium
