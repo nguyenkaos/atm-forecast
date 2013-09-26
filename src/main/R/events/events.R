6870011379db54a4f3f85c267e0a3daf721461b5
nickwallen <nick@nickallen.org>
2013-09-26 11:08:34 -0400

    Completed the effort to only query for events for unique locations.  There may be multiple ATMs at the same location so this helps avoid repeatedly fetching and storing duplicate events.

--- src/main/R/events/events.R
+++ src/main/R/events/events.R
@@ -1,55 +1,56 @@
 library("data.table")
-library("dplyr")
+library("plyr")
 
 source("seatgeek.R")
 
+#
+# Grab a set of ATMs that have sufficient data to work with.
+#
+fetchATMs <- function() {
+    
+    # get all ATMs that have sufficient usage history
+    usage <- data.table(readRDS("../../resources//usage-all.rds"), key="atm")
+    usage <- usage[, list(history=length(usage)), by=atm]
+    usage <- usage[history>30,,]
+    
+    # get all ATMs that have location data
+    profiles <- data.table(readRDS("../../resources/profiles.rds"), by="atm")
+    profiles <- subset(profiles, select=c(atm, latitude, longitude)) 
+    profiles$latitude[profiles$latitude==0] <- NA
+    profiles$longitude[profiles$longitude==0] <- NA
+    profiles <- subset(profiles, !is.na(longitude))
+    
+    # final set of ATMs that have sufficient history and location data
+    profiles <- merge(profiles, usage, by="atm")
+    
+    # assign each a 'geo.id' based on lat/long
+    profiles$geo.id <- id(profiles[,c("longitude","latitude"), with=F], drop=T)
+    return(profiles)
+}
 
 #
-# TODO - remove duplicate locations and store another geo.id -> events
-# table
+# Extract a set of unique 'points of interest'
 #
-profiles <- readRDS("../../resources/profiles.rds")
-profiles <- subset(profiles, select=c(atm, latitude, longitude)) 
-
-# remove those with invalid geo data
-profiles$latitude[profiles$latitude==0] <- NA
-profiles$longitude[profiles$longitude==0] <- NA
-profiles <- subset(profiles, !is.na(longitude))
-
-# assign each atm to a group based on geo
-# TODO - this is not returning unique groups
-profiles$geo.id <- id(profiles[c("longitude","latitude")], drop=T)
-
-# analyze the groups - how much time are we saving here?
-profiles <- data.table(profiles)
-geos <- profiles[, list(latitude, longitude, count=length(atm)), by=geo.id]
-geos <- geos[order(-count)]
+extractPOI <- function(atms) {
+    atms <- subset(atms, select=c(geo.id, longitude, latitude))
+    poi <- atms[!duplicated(atms)]
+    setkey(poi, geo.id)
+    return(poi)
+}
 
 #
 # Fetches events 'close' to each of the ATMs in the fleet.
 #
-fetchEvents <- function(parallel=F) {
+fetchEvents <- function(poi, api=SeatGeekR$new(), parallel=F) {
     
-    # fetch the profiles for each ATM
-    profiles <- readRDS("../../resources/profiles.rds")
-    profiles <- subset(profiles, select=c(atm, latitude, longitude)) 
-    profiles <- data.table(profiles)
+    # grab events near each 'point of interest'
+    events <- ddply(poi, "geo.id", function(p) {
+        loginfo("fetching events for geo.id '%s'", p$geo.id)
+        events <- tryCatch(
+            api$events(lat=p$latitude, lon=p$longitude, range="1mi"), 
+            error=function(e) logerror("error for geo.id '%s': %s", p$geo.id, e))
+    }, .parallel=parallel)
     
-    # grab events near any of the ATMs
-    geek <- SeatGeekR$new()
-    fetch <- function(p) {
-        loginfo("fetching events for atm %s", p$atm)
-        
-        # lat/lon required
-        events <- NULL
-        if(!is.na(p$latitude) && !is.na(p$longitude)) {
-            events <- tryCatch(
-                geek$events(lat=p$latitude, lon=p$longitude, range="1mi"), 
-                error=function(e) logerror("error for %s: %s", p$atm, e))
-        }
-    }
-    events <- profiles[, fetch(.SD), by=atm]
-        
     # clean-up the resulting events data
     events <- rename(events, c("id"="event.id"))
 }
