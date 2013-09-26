8fcd8572b0f6afb4380bb525b234ca06588f8500
nickwallen <nick@nickallen.org>
2013-09-26 11:06:47 -0400

    Added command-line option to either run in parallel or sequential.

--- src/main/R/events/main.R
+++ src/main/R/events/main.R
@@ -1,7 +1,14 @@
 #!/usr/bin/env Rscript
 
-library("doSNOW")
-registerDoSNOW(cl <- makeCluster(4, type="SOCK", outfile=""))
+# defines the options/arguments
+library("optparse")
+all_options <- list(
+    make_option(c("-p", "--parallel"), 
+                action="store_true",
+                help="Run with a parallel backend enabled",
+                default=F)
+)
+opts <- parse_args(OptionParser(option_list=all_options))
 
 library("logging")
 library("plyr")
@@ -9,26 +16,45 @@ source("seatgeek.R")
 source("events.R")
 basicConfig(level="INFO")
 
-# initialize each of the slaves
-clusterEvalQ(cl, {
-    library("logging")
-    library("plyr")
-    source("seatgeek.R")    
-    source("events.R")
-    basicConfig(level="INFO")
-})
-
-tryCatch({
-    events <- fetchEvents(parallel=T)
-    saveRDS(events, "seatgeek-events.rds")
+#
+# Drives the process of fetching events for all ATMs.
+#
+main <- function() {
+    atms <- fetchATMs()
+    saveRDS(atms, "atms-to-geo.rds")
     
-}, finally = {
-    stopCluster(cl)
-})
+    poi <- extractPOI(atms)
+    events <- fetchEvents(poi)
+    saveRDS(events, "geo-to-events.rds")
+}
 
-
-
-
-
-
-
+#
+# should the process by run in parallel?
+#
+if(!opts$parallel) {
+    main()
+    
+} else {
+    loginfo("execution proceeding in parallel")
+    
+    # execute in parallel
+    library("doSNOW")
+    registerDoSNOW(cl <- makeCluster(4, type="SOCK", outfile=""))
+    
+    # initialize each of the slaves
+    clusterEvalQ(cl, {
+        library("logging")
+        library("plyr")
+        source("seatgeek.R")    
+        source("events.R")
+        basicConfig(level="INFO")
+    })
+    
+    # do stuff, then stop the cluster
+    tryCatch({
+        main()
+        
+    }, finally = {
+        stopCluster(cl)
+    })
+}
