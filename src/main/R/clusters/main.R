50685e78389989bf6dc71e1036dc7e6603f5b1d5
Nick Allen <nick@nickallen.org>
2013-10-15 12:06:13 -0400

    Starting on the clustering effort.

---
+++ src/main/R/clusters/main.R
@@ -0,0 +1,17 @@
+source("fetch.R")
+
+
+main <- function() {
+    
+    # read in the deposits feature set
+    
+    
+    # read in cluster data set
+    
+    # merge with existing feature set
+    
+    # forecast by cluster
+    
+    # score
+    
+}
