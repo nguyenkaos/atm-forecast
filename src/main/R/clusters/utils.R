4687fe22b5e2c2c44825b3dceb720d12991497d2
Nick Allen <nick@nickallen.org>
2013-10-16 12:03:15 -0400

    Adding some utility functions.

---
+++ src/main/R/clusters/utils.R
@@ -0,0 +1,28 @@
+
+library("plyr")
+library("data.table")
+
+#
+# transforms the historical usage data (either deposits or withdrawals) into a
+# time series.
+#
+as.ts <- function(history) {
+    
+    # tranform the 'long' history to a 'wide' time series
+    history.ts <- dlply( history, 
+                         "atm", 
+                         function(byAtm) { 
+                             rev(as.vector(byAtm$usage)) 
+                         }, 
+                         .progress = "text")
+    
+    # time series will be of different lengths; need most recent data 
+    # first to allow ATMs with different life spans to be compared
+    history.dt <- data.table(atm = names(history.ts), time.series = I(history.ts), key="atm")
+}
+
+basename.only <- function (path) {
+    base <- basename(path)
+    matches <-  regexpr("[^.]+", base)
+    regmatches(base, matches)
+}
