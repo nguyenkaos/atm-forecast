8f6f0a0928261035fc48d7ffbc7bd455d2414828
Nick Allen <nick@nickallen.org>
2013-10-23 08:40:17 -0400

    Simplified the way that results are summarized.

--- src/main/R/forecast/deposits.R
+++ src/main/R/forecast/deposits.R
@@ -5,28 +5,31 @@ library("optparse")
 # defines the options/arguments
 getOptions <- function() {
     all_options <- list(
-        make_option(c("--forecastOut"),
-                    help    = "How far out to forecast in days [default: %default]",
-                    default = 120),
-        make_option(c("--splitAt"),
-                    help    = "Date at which to split training vs test [default: %default]",
-                    default = "2013-07-16"),
-        make_option(c("-l", "--logLevel"),
-                    help    = "Level of logging [default: %default]",
-                    default = "INFO"),  
-        make_option(c("--subset"),
-                    help    = "An expression to identify a subset of ATMs to forecast [default: %default (all)]",
-                    default = "T==T"),
-        make_option(c("-d", "--dataDir"),
-                    help    = "Directory containing the data files [default: %default]",
-                    default = "../../resources"),
-        make_option(c("--historyFile"), 
-                    help    = "RDS file containing the ATM history [default: %default]",
-                    default = "deposits-micro.rds"),
-        make_option(c("e", "--export"),
-                    help    = "Export the forecast [default: %default]",
-                    default = FALSE)
-        
+        make_option (c("--forecastOut"),
+                     help    = "How far out to forecast in days [default: %default]",
+                     default = 120),
+        make_option (c("--splitAt"),
+                     help    = "Date at which to split training vs test [default: %default]",
+                     default = "2013-07-16"),
+        make_option (c("-l", "--logLevel"),
+                     help    = "Level of logging [default: %default]",
+                     default = "INFO"),  
+        make_option (c("--subset"),
+                     help    = "An expression to identify a subset of ATMs to forecast [default: %default (all)]",
+                     default = "T==T"),
+        make_option (c("-d", "--dataDir"),
+                     help    = "Directory containing the data files [default: %default]",
+                     default = "../../resources"),
+        make_option (c("--historyFile"), 
+                     help    = "RDS file containing the ATM history [default: %default]",
+                     default = "deposits-micro.rds"),
+        make_option (c("e", "--export"),
+                     help    = "Export the forecast [default: %default]",
+                     default = FALSE),
+        make_option (c("--verbose"),
+                     help    = "The verbosity of the champion/challenger comparison [default: %default]",
+                     default = FALSE,
+                     action  = "store_true")
     )
     opts <- parse_args (OptionParser (option_list = all_options))
 }
@@ -53,8 +56,8 @@ source("score.R")
 # initialization
 basicConfig (level = loglevels [opts$logLevel])
 
-deposits.cache <- sprintf("%s-features", basename.only(opts$historyFile))
-deposits <- cache(deposits.cache, {
+deposits.cache <- sprintf("%s-features", basename.only (opts$historyFile))
+deposits <- cache (deposits.cache, {
     
     # fetch and clean the input data
     fetch( history.file = opts$historyFile,
@@ -62,15 +65,14 @@ deposits <- cache(deposits.cache, {
            data.dir     = opts$dataDir)
 })
 
-challenger.cache <- sprintf ("%s-challenger", basename.only(opts$historyFile))
+challenger.cache <- sprintf ("%s-challenger", basename.only (opts$historyFile))
 challenger <- cache (challenger.cache, {
     
+    # train and fit a model
     fit.cache <- sprintf ("%s-fit", basename.only (opts$historyFile))
     deposits[ 
         # include only those ATMs that pass the 'subset' expression (optional)
         eval (parse (text = opts$subset)), 
-        
-        # train and fit a model
         `:=` (
             model = "challenger",
             usage.hat = trainAndPredict (
@@ -95,7 +97,6 @@ challenger <- cache (challenger.cache, {
 })
 
 # compare the champion and challenger models; the test period is August
-loginfo("comparing the champion to the challenger")
 compare.start <- "2013-07-31"
 compare.end <- "2013-09-01"
 champion.file <- "../../resources/deposits-champion.rds"
@@ -116,52 +117,30 @@ challenger <- challenger [
     colnames(champion), 
     with = FALSE]
 
-# create a day-by-day comparison of the models
-model.compare.details <- merge (
-    subset (champion, select = c(atm, trandate, usage, usage.hat)), 
-    subset (challenger, select = c(atm, trandate, usage.hat)), 
-    by = c("atm", "trandate"),
-    suffixes = c(".chmp",".chal")) 
+# combine each into a single data set for further comparison
+models.all <- rbindlist (list (champion, challenger))
 
-# score each of the atm-days for the challenger
-model.challenger.details <- challenger [
-    , list (
-        usage,
-        usage.hat,
-        model        = "challenger",
-        points       = points (usage, usage.hat),
-        mape         = mape (usage, usage.hat),
-        mse          = mse (usage, usage.hat),
-        rmse         = rmse (usage, usage.hat)
-    ), by = list(atm, trandate)]
-
-# score each of the atm-days for the champion
-model.champion.details <- champion [
-    , list (
-        usage,
-        usage.hat,
-        model        = "champion",
-        points       = sum (points (usage, usage.hat)),
-        mape         = mape (usage, usage.hat),
-        mse          = mse (usage, usage.hat),
-        rmse         = rmse (usage, usage.hat)
-    ), by = list(atm, trandate)]
-
-# combine the champion and challenger for comparison over the test period
-models <- rbindlist (list (champion, challenger))
-model.compare.summary <- models [, list (
-    points       = sum (points (usage, usage.hat)),
-    mape         = mape (usage, usage.hat),
-    mse          = mse (usage, usage.hat),
-    rmse         = rmse (usage, usage.hat),
-    under.05.ape = ape.between(usage, usage.hat, 0.00, 0.05),
-    under.10.ape = ape.between(usage, usage.hat, 0.05, 0.10),
-    under.20.ape = ape.between(usage, usage.hat, 0.10, 0.20),
-    over.20.ape  = ape.between(usage, usage.hat, 0.20, Inf),
-    total.obs    = length (usage),
-    total.atm    = length (unique (atm))
-), by = list(model, month(trandate)) ]
-write.csv(model.compare.summary, sprintf("model-comparison-%s.csv", today()), row.names = T)
+if (opts$verbose) {
+    
+    # score each atm-day for the champion and challenger
+    scores.daily <- models.all [
+        , list (
+            usage,
+            usage.hat,
+            model,
+            points       = points (usage, usage.hat),
+            mape         = mape (usage, usage.hat),
+            mse          = mse (usage, usage.hat),
+            rmse         = rmse (usage, usage.hat)
+        ), by = list(atm, trandate)]
+    
+    # export the daily scores of both models
+    export.file <- sprintf("deposit-daily-%s.csv", today())
+    write.csv(scores.daily, export.file, row.names = FALSE)
+    loginfo("exporting daily scores to '%s'", export.file)
+    
+    # TODO - DEFECT - each model working with difference usage actuals!
+}
 
 # should the forecast be exported?
 if(opts$export) {
@@ -176,13 +155,26 @@ if(opts$export) {
         ), ]
     
     # export the forecast to a csv file
-    filename <- sprintf("forecast-%s.csv", today())
+    filename <- sprintf("deposit-forecast-%s.csv", today())
     write.csv(forecast, filename)
     loginfo("forecasting exported to %s", filename)
 }
 
-# output results
-model.challenger.details[]
-model.champion.details[]
-model.compare.details[]
-model.compare.summary[]
+# create a summary of the differences between champion and challenger
+models.all.summary <- models.all [, list (
+    points       = sum (points (usage, usage.hat)),
+    mape         = mape (usage, usage.hat),
+    mse          = mse (usage, usage.hat),
+    rmse         = rmse (usage, usage.hat),
+    under.05.ape = ape.between(usage, usage.hat, 0.00, 0.05),
+    under.10.ape = ape.between(usage, usage.hat, 0.05, 0.10),
+    under.20.ape = ape.between(usage, usage.hat, 0.10, 0.20),
+    over.20.ape  = ape.between(usage, usage.hat, 0.20, Inf),
+    total.obs    = length (usage),
+    total.atm    = length (unique (atm))
+), by = list(model, month(trandate)) ]
+
+# export the summary
+write.csv(models.all.summary, sprintf("deposit-summary-%s.csv", today()), row.names = T)
+models.all.summary[]
+
