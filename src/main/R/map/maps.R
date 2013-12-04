2a68c514c3f3b5f18839d8f4d41e95cfc8268c6e
unknown <J918358@LB22614WW.NAEAST.AD.JPMORGANCHASE.com>
2013-12-04 09:38:00 -0500

    Playing with different ways to map

--- src/main/R/map/maps.R
+++ src/main/R/map/maps.R
@@ -5,7 +5,7 @@ library (data.table)
 #
 # find geographic details of a location based on a search term
 #
-geoCode <- function (address) {
+geocode <- function (address) {
     
     url <- function(address, root = "http://maps.google.com/maps/api/geocode/") {
         URLencode (paste (root, "json", "?address=", address, "&sensor=", "false", sep = ""))
@@ -25,45 +25,133 @@ geoCode <- function (address) {
         address.clean <- result$formatted_address
         
         # extract the location details
-        geo <- result$geometry
-        lon <- geo$location$lng
-        lat <- geo$location$lat
-        type <- geo$location_type
+        geo  <- result$geometry
+        location <- list(lon     = geo$location$lng, 
+                         lat     = geo$location$lat, 
+                         type    = geo$location_type, 
+                         address = address.clean)
+    } else {
+        stop ("unable to find address: %s", response)
         
-        location <- list(lon = lon, lat = lat, type = type, address = address.clean)
-    } 
+    }
     
     return (location)
 }
 
 #
-# plot a set of points that fit within the map (bad description)
+# plots points of interest (poi) within a map.  the only requirement is that
+# poi must contain a "lon" and "lat" column.
 #
-plotAtms <- function (address, zoom = "auto", points, alpha = 0.67, size = 10) {
+map.color <- function (address, poi, zoom = "auto", alpha = 0.67, size = 10) {
     
     # start with initial geographic locale
-    location <- geoCode (address)
+    location <- geocode (address)
     map.data <- get_map (location = c(location$lon, location$lat), zoom = zoom, maptype = 'roadmap')
-    map <- ggmap (map.data)
+    map.base <- ggmap (map.data, extent = "panel")
     
-    # find the range of lon/lat defined within the map
-    lon.min <- min (map$data$lon)
-    lon.max <- max (map$data$lon)
-    lat.min <- min (map$data$lat)
-    lat.max <- max (map$data$lat)
+    # find the range of lon/lat visible within the map
+    lon.min <- min (map.base$data$lon)
+    lon.max <- max (map.base$data$lon)
+    lat.min <- min (map.base$data$lat)
+    lat.max <- max (map.base$data$lat)
     
     # find all points within the borders of the map
-    in.map <- points [ longitude > lon.min & longitude < lon.max & latitude > lat.min & latitude < lat.max ]
+    poi.in.map <- poi [ lon > lon.min & lon < lon.max & lat > lat.min & lat < lat.max ]
     
     # plot those points that fit within the map
-    map + 
-        geom_point(data = in.map, 
-                   aes(x = longitude, y = latitude, colour = withdrawals.past.2wk), alpha = alpha, size = size) + 
+    map.base + 
+        geom_point(data = poi.in.map, aes(x = lon, y = lat, colour = withdrawals.past.2wk), alpha = alpha, size = size) + 
         scale_colour_gradient (low = "blue", high = "red")
 }
 
-# grab the profile data for the fleet
-profiles <- readRDS("../../resources/profiles.rds")
-profiles <- data.table(profiles, key = "atm")
+#
+# plots points of interest (poi) within a map.  the only requirement is that
+# poi must contain a "lon" and "lat" column.
+#
+map.size <- function (address, poi, zoom = "auto", size.range = c(0,20)) {
+    
+    # start with initial geographic locale
+    location <- geocode (address)
+    map.data <- get_map (location = c(location$lon, location$lat), zoom = zoom, maptype = 'roadmap')
+    map.base <- ggmap (map.data, extent = "panel")
+    
+    # find the range of lon/lat visible within the map
+    lon.min <- min (map.base$data$lon)
+    lon.max <- max (map.base$data$lon)
+    lat.min <- min (map.base$data$lat)
+    lat.max <- max (map.base$data$lat)
+    
+    # find all points within the borders of the map
+    poi.in.map <- poi [ lon > lon.min & lon < lon.max & lat > lat.min & lat < lat.max ]
+    
+    # plot those points that fit within the map
+    map.base + 
+        geom_point (data = poi.in.map, aes(x      = lon, 
+                                           y      = lat, 
+                                           colour = withdrawals.past.2wk, 
+                                           size   = withdrawals.past.2wk,
+                                           alpha  = withdrawals.past.2wk), alpha = 0.5) + 
+        scale_colour_gradient (low = "blue", high = "red") + 
+        scale_size_continuous (range = size.range)
+}
 
-plotAtms ("Chicago, IL", 2, profiles)
+#
+# plots points of interest (poi) within a map.  the only requirement is that
+# poi must contain a "lon" and "lat" column.
+#
+map.zip <- function (address, poi, zoom = "auto", size.range = c(0,20)) {
+    
+    # start with initial geographic locale
+    location <- geocode (address)
+    map.data <- get_map (location = c(location$lon, location$lat), zoom = zoom, maptype = 'roadmap')
+    map.base <- ggmap (map.data, extent = "panel")
+    
+    # find the range of lon/lat visible within the map
+    lon.min <- min (map.base$data$lon)
+    lon.max <- max (map.base$data$lon)
+    lat.min <- min (map.base$data$lat)
+    lat.max <- max (map.base$data$lat)
+    
+    # find all points within the borders of the map
+    poi.in.map <- poi [ lon > lon.min & lon < lon.max & lat > lat.min & lat < lat.max ]
+    
+    # plot those points that fit within the map
+    map.base + 
+        geom_point (data = poi.in.map, aes (x      = lon, 
+                                            y      = lat, 
+                                            colour = withdrawals.past.2wk, 
+                                            size   = withdrawals.past.2wk,
+                                            alpha  = withdrawals.past.2wk), alpha = 0.5) + 
+        scale_colour_gradient (low = "blue", high = "red") + 
+        scale_size_continuous (range = size.range)
+}
+
+map.faults <- function (address, poi, zoom = "auto", size.range = c(0,20)) {
+    
+    # start with initial geographic locale
+    location <- geocode (address)
+    loginfo("'%s' -> '%s'", address, location)
+    
+    map.data <- get_map (location = c(location$lon, location$lat), zoom = zoom, maptype = 'roadmap')
+    map.base <- ggmap (map.data, extent = "panel")
+    
+    # find the range of lon/lat visible within the map
+    lon.min <- min (map.base$data$lon)
+    lon.max <- max (map.base$data$lon)
+    lat.min <- min (map.base$data$lat)
+    lat.max <- max (map.base$data$lat)
+    
+    # find all points within the borders of the map
+    poi.in.map <- poi [ lon > lon.min & lon < lon.max & lat > lat.min & lat < lat.max ]
+    
+    # plot those points that fit within the map
+    map.base + 
+        geom_point (data = poi.in.map, aes (x      = lon, 
+                                            y      = lat, 
+                                            colour = fault.per.usage, 
+                                            size   = fault.per.usage), alpha = 0.5) + 
+        scale_colour_gradient (low = "blue", high = "red") + 
+        scale_size_continuous (range = size.range)
+}
+
+
