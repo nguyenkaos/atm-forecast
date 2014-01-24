f175a647b665f648890cf0815c75f93a34c621ba
unknown <j918358@LB22614WW.NAEAST.AD.JPMORGANCHASE.com>
2014-01-24 17:14:46 -0500

    Adding the data needed for the schedules monte carlo simulation.

---
+++ src/main/R/common/create-forecasts.R
@@ -0,0 +1,44 @@
+
+library ("data.table")
+
+#
+# create the withdrawals forecast data set from the SAS original
+#
+input <- "C:/Users/j918358/Desktop/forecast_withdrawals.csv"
+forecast <- fread (input)
+forecast [, `:=` (
+    trandate  = as.Date (trandate, "%d%b%y"),
+    usage     = as.numeric (gsub ("[$,]", "", usage)),
+    p_usg_adj = as.numeric (gsub ("[$,]", "", p_usg_adj)),
+    lb        = as.numeric(lb),
+    ub        = as.numeric(ub)
+)]
+
+setnames (forecast, "p_usg_adj", "usage.hat")
+setnames (forecast, "lb", "lower.bound")
+setnames (forecast, "ub", "upper.bound")
+setkeyv (forecast, c("atm","trandate"))
+
+saveRDS (forecast, "../../resources/forecast-withdrawals.rds")
+
+#
+# create the deposits forecast data set from the SAS original
+#
+input <- "C:/Users/j918358/Desktop/forecast_deposits.csv"
+forecast <- fread (input)
+forecast [, `:=` (
+    trandate  = as.Date (trandate, "%d%b%y"),
+    billcnt   = as.numeric (billcnt),
+    p_dep_adj = as.numeric (gsub ("[$,]", "", p_dep_adj)),
+    lb        = as.numeric(lb),
+    ub        = as.numeric(ub)
+)]
+
+setnames (forecast, "billcnt", "bills")
+setnames (forecast, "p_dep_adj", "bills.hat")
+setnames (forecast, "lb", "lower.bound")
+setnames (forecast, "ub", "upper.bound")
+setkeyv (forecast, c("atm","trandate"))
+
+saveRDS (forecast, "../../resources/forecast-deposits.rds")
+
